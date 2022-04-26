4ad371dcdb294f54723903da9f35da6133f75116
thomasfritzboger <cph-tf67@cphbusiness.dk>
2022-04-26 11:57:36 +0200

    ny fil

---
+++ src/main/java/dat/startcode/control/ThomasCommand.java
@@ -0,0 +1,19 @@
+package dat.startcode.control;
+
+import dat.startcode.model.exceptions.DatabaseException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+public class ThomasCommand extends Command
+{
+    @Override
+    String execute(HttpServletRequest request, HttpServletResponse response) throws DatabaseException
+    {
+        String hilsen = "Her kan du se hvordan man sender indhold til en jsp-side.";
+        request.setAttribute("hilsen", hilsen);
+        return "about";
+    }
+}
