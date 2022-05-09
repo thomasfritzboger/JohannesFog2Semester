666968b3a68872ea08bee22b7a11222f2882756b
Andreas Fritzbøger <andreasfritzboger@live.dk>
2022-05-09 11:21:22 +0200

    opdaterer fogTestDB.sql

--- src/main/resources/fogTestDB.sql
+++ src/main/resources/fogTestDB.sql
@@ -1,7 +1,9 @@
+CREATE SCHEMA IF NOT EXISTS `fog_test` ;
 create table fog_test.carport like fog.carport;
 create table fog_test.coverage like fog.coverage;
 create table fog_test.material_line like fog.material_line;
 create table fog_test.product like fog.product;
 create table fog_test.productvariant like fog.productvariant;
 create table fog_test.shed like fog.shed;
+create table fog_test.usement like fog.usement;
 create table fog_test.user like fog.user;
