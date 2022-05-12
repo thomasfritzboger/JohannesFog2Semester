5d611eaedfd311eb12382bf752f72b887a0c411f
thomasfritzboger <cph-tf67@cphbusiness.dk>
2022-05-12 09:53:27 +0200

    Rebase Thomas Branch on main

---
+++ src/main/java/dat/startcode/model/dtos/OrderLineDTO.java
@@ -0,0 +1,36 @@
+package dat.startcode.model.dtos;
+
+import java.time.LocalDate;
+
+public class OrderLineDTO {
+
+    private String productDescription;
+    private int length;
+    private int amount;
+    private String unitScale;
+    private String usementDescription;
+
+    private double totalPrice;
+
+    public OrderLineDTO(String productDescription, int length, int amount, String unitScale, String usementDescription, double totalPrice) {
+        this.productDescription = productDescription;
+        this.length = length;
+        this.amount = amount;
+        this.unitScale = unitScale;
+        this.usementDescription = usementDescription;
+        this.totalPrice = totalPrice;
+    }
+
+
+    @Override
+    public String toString() {
+        return "OrderLineDTO{" +
+                "productDescription='" + productDescription + '\'' +
+                ", length=" + length +
+                ", amount=" + amount +
+                ", unitScale='" + unitScale + '\'' +
+                ", usementDescription='" + usementDescription + '\'' +
+                ", totalPrice=" + totalPrice +
+                '}';
+    }
+}
