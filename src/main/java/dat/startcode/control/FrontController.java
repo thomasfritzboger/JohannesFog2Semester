7dd40a034998a930d3266fe44aa18c0146c6367e
Andreas Fritzbøger <andreasfritzboger@live.dk>
2022-05-19 12:49:15 +0200

    Tilføjer oversætter

--- src/main/java/dat/startcode/control/FrontController.java
+++ src/main/java/dat/startcode/control/FrontController.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.util.Locale;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -27,6 +28,7 @@ public class FrontController extends HttpServlet {
         {
             request.setCharacterEncoding("UTF-8");
             response.setCharacterEncoding("UTF-8");
+            Locale.setDefault(new Locale("en_US"));
 
             Command action = Command.from( request );
             String view = action.execute( request, response );
